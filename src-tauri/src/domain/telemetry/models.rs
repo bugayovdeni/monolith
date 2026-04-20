fe11aed573644e593a85abde2544b7960387665a
Bodden007 <denis-bodrov@mail.ru>
2026-04-20 16:27:06 +0500

    Added telemetry

---
+++ src-tauri/src/domain/telemetry/models.rs
@@ -0,0 +1,13 @@
+use serde::{Deserialize, Serialize};
+
+#[derive(Debug, Clone, Serialize, Deserialize, PartialEq)]
+pub struct MeasurementSnapshot {
+    pub timestamp_ms: i64,
+    pub value: f64,
+}
+
+#[derive(Debug, Clone, Serialize, Deserialize, PartialEq)]
+pub struct ChartPoint {
+    pub x: i64,
+    pub y: f64,
+}
